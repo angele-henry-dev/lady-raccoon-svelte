7f3e75c1a95e7dd8b1de8f39d7cd2af971046ec9
Angèle Henry <angele.henry@outlook.com>
2025-01-24 17:25:22 +0100

    Add the main title with animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,5 +9,61 @@
 </svelte:head>
 
 <BackgroundStars />
-<h1>Welcome to Plop</h1>
+
+<div class='text-center my-20 container mx-auto'>
+    <div class="text-center main-title">
+      Transformez votre site
+      <div class="main-title-container flex flex-row justify-center gap-3 highlight">
+        <div class="main-title-container__">
+          <ul class="main-title-container__list">
+            <li class="main-title-container__list__item">accessible</li>
+            <li class="main-title-container__list__item">rapide</li>
+            <li class="main-title-container__list__item">visible</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</div>
+
 <BackgroundRetro />
+
+<style>
+.main-title {
+    font-size: 3em;
+}
+  
+.main-title-container {
+    height: 1em;
+}
+.main-title-container .main-title-container__ {
+    overflow: hidden;
+}
+.main-title-container__list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    animation-name: swipe;
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
+}
+@media (prefers-reduced-motion: reduce) {
+    .main-title-container__list {
+      animation: none;
+    }
+}
+@keyframes swipe {
+    0%,
+    12%,
+    100% {
+      transform: translate3d(0, -5%, 0);
+    }
+    33%,
+    46% {
+      transform: translate3d(0, -40%, 0);
+    }
+    66%,
+    79% {
+      transform: translate3d(0, -70%, 0);
+    }
+}
+</style>
