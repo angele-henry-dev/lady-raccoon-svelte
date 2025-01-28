93529785c39620958026094fac334141f3ce46df
Angèle Henry <angele.henry@outlook.com>
2025-01-28 20:39:54 +0100

    Design offer (first one)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,18 @@
 	<meta name="Description" content="Boostez votre impact avec un site rapide, accessible et optimisé" />
 </svelte:head>
 
+<BackgroundStars />
+
 <div class="dark hero w-full flex flex-col justify-around">
   <div class="container mx-auto">
     <p class="text-4xl md:text-6xl mb-10">Boostez votre impact avec un site <span class="highlight">rapide, accessible et optimisé.</span></p>
     <Audit />
-    <Arrow />
   </div>
 </div>
 
-<div class="mb-20 container mx-auto">
+<BackgroundRetro />
+
+<div class="my-20 container mx-auto">
   <h1>Saviez-vous que...</h1>
   <div class="mt-10 mb-20 flex flex-row flex-wrap gap-10 justify-center">
     <div class="card w-64">
@@ -47,12 +50,12 @@
   <div class="mt-10 flex flex-row flex-wrap gap-10 justify-center">
     <div class="card w-64">
       <div class="card-info">
-        <p>Un site conforme aux <span class="highlight">normes WCAG 2.1</span>, niveaux AA et AAA.</p>
+        <p>Une meilleure <span class="highlight">expérience utilisateur</span> pour tous vos visiteurs.</p>
       </div>
     </div>
     <div class="card w-64">
       <div class="card-info">
-        <p>Une meilleure <span class="highlight">expérience utilisateur</span> pour tous vos visiteurs.</p>
+        <p>Un site conforme aux <span class="highlight">normes WCAG 2.1</span>, niveaux AA et AAA.</p>
       </div>
     </div>
     <div class="card w-64">
@@ -69,79 +72,97 @@
 </div>
 <Audit classes="mx-10 pt-10" />
 
-<div class='my-20 container mx-auto rounded-3xl border p-5'>
-  <h1>Pourquoi travailler avec moi ?</h1>
-
-  <h2>1. Expertise technique</h2>
-  <ul>
-    <li>Plus de 10 ans d’expérience en développement web</li>
-    <li>Spécialisation en accessibilité web et conformité WCAG</li>
-  </ul>
-  
-  <h2>2. Approche personnalisée</h2>
-  <ul>
-    <li>Analyse adaptée à vos besoins et à vos utilisateurs</li>
-    <li>Solutions sur mesure pour répondre à vos enjeux spécifiques</li>
-  </ul>
-  
-  <h2>3. Implication durable</h2>
-  <ul>
-    <li>Accompagnement de vos équipes pour intégrer les bonnes pratiques sur le long terme</li>
-    <li>Support continu pour garantir une accessibilité optimale</li>
-  </ul>
+<div class='inverted my-20 py-10'>
+  <div class="container mx-auto py-10">
+    <h1>Pourquoi travailler avec moi ?</h1>
+    <div class="content border-l-4 pl-5 mt-5">
+      <h2>Expertise technique</h2>
+      <ul>
+        <li>Plus de 10 ans d’expérience en développement web</li>
+        <li>Spécialisation en accessibilité web et conformité WCAG</li>
+      </ul>
+      
+      <h2>Approche personnalisée</h2>
+      <ul>
+        <li>Analyse adaptée à vos besoins et à vos utilisateurs</li>
+        <li>Solutions sur mesure pour répondre à vos enjeux spécifiques</li>
+      </ul>
+      
+      <h2>Implication durable</h2>
+      <ul>
+        <li>Accompagnement de vos équipes pour intégrer les bonnes pratiques sur le long terme</li>
+        <li>Support continu pour garantir une accessibilité optimale</li>
+      </ul>
+    </div>
+  </div>
 </div>
 <Audit classes="mx-10 pt-10" />
 
 <div class='my-20 container mx-auto'>
   <h1>Mon offre en détail</h1>
 
-  <h2>1. Audit d’accessibilité</h2>
-  <ul>
-    <li><b>Objectif:</b> Identifier les problèmes d’accessibilité et proposer des solutions concrètes.</li>
-    <li>
-      <b>Ce que j’analyse :</b>
-      <ul>
-        <li>Structure HTML et sémantique</li>
-        <li>Couleurs et contrastes</li>
-        <li>Navigation clavier et interactions</li>
-        <li>Conformité avec les normes WCAG 2.1 (AA et AAA)</li>
-      </ul>
-    </li>
-    <li><b>Livrable :</b> Un rapport détaillé avec un plan d’action priorisé.</li>
-  </ul>
+  <div class="mt-10 flex flex-row gap-5 justify-around">
+    <div class="card-offer">
+      <div class="content">
+        <div class="title">Audit détaillé d’accessibilité</div>
+        <div class="description">
+          <p><b>Objectif:</b> Identifier les problèmes d’accessibilité et proposer des solutions concrètes.</p>
+          <div class="mt-3"><b>Ce que j’analyse :</b>
+            <ul>
+              <li>Structure HTML et sémantique</li>
+              <li>Couleurs et contrastes</li>
+              <li>Navigation clavier et interactions</li>
+              <li>Conformité avec les normes WCAG 2.1 (AA et AAA)</li>
+            </ul>
+          </div>
+          <p><b>Livrable :</b> Un rapport détaillé avec un plan d’action priorisé.</p>
+        </div>
+      </div>
+        <button>
+          Buy now
+        </button>
+    </div>
 
-  <h2>2. Corrections et mise en conformité</h2>
-  <ul>
-    <li><b>Objectif:</b> Appliquer les changements nécessaires pour rendre votre site conforme.</li>
-    <li>
-      <b>Ce que je fais :</b>
-      <ul>
-        <li>Corrections HTML/CSS sémantiques</li>
-        <li>Optimisation des contrastes</li>
-        <li>Ajout d’éléments ARIA pour les lecteurs d’écran</li>
-        <li>Tests après modifications pour valider la conformité</li>
-      </ul>
-    </li>
-    <li><b>Livrable :</b> Un site conforme aux normes WCAG 2.1, accessible à tous.</li>
-  </ul>
+    <div class="card-offer">
+      <div class="content">
+        <div class="title">Corrections et mise en conformité</div>
+        <div class="description">
+          <p><b>Objectif:</b> Appliquer les changements nécessaires pour rendre votre site conforme.</p>
+          <div class="mt-3"><b>Ce que j’analyse :</b>
+            <ul>
+              <li>Corrections HTML/CSS sémantiques</li>
+              <li>Optimisation des contrastes</li>
+              <li>Ajout d’éléments ARIA pour les lecteurs d’écran</li>
+              <li>Tests après modifications pour valider la conformité</li>
+            </ul>
+          </div>
+          <p><b>Livrable :</b> Un site conforme aux normes WCAG 2.1, accessible à tous.</p>
+        </div>
+      </div>
+        <button>
+          Buy now
+        </button>
+    </div>
 
-  <h2>3. Formation des équipes techniques</h2>
-  <ul>
-    <li><b>Objectif:</b> Sensibiliser vos développeurs au développement accessible.</li>
-    <li>
-      <b>Programme :</b>
-      <ul>
-        <li>Introduction aux WCAG et principes de base</li>
-        <li>Démonstration d’outils et tests pratiques</li>
-        <li>Cas concrets appliqués à vos projets</li>
-      </ul>
-    </li>
-    <li><b>Livrable :</b>
-      <ul>
-        <li>Supports de formation PDF</li>
-        <li>Accès à des ressources pratiques pour aller plus loin</li>
-      </ul>
-    </li>
-  </ul>
+    <div class="card-offer">
+      <div class="content">
+        <div class="title">Formation des équipes techniques</div>
+        <div class="description">
+          <p><b>Objectif:</b> Sensibiliser vos développeurs au développement accessible.</p>
+          <div class="mt-3"><b>Ce que j’analyse :</b>
+            <ul>
+              <li>Introduction aux WCAG et principes de base</li>
+              <li>Démonstration d’outils et tests pratiques</li>
+              <li>Cas concrets appliqués à vos projets</li>
+            </ul>
+          </div>
+          <p><b>Livrable :</b> Supports de formation PDF et accès à des ressources pratiques pour aller plus loin.</p>
+        </div>
+      </div>
+        <button>
+          Buy now
+        </button>
+    </div>
+  </div>
 </div>
 <Audit classes="mx-10 mb-20" />
