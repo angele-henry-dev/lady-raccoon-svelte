74f7d1fafe791e429771e15523c5cfe06a22438a
Angèle Henry <angele.henry@outlook.com>
2025-03-31 18:42:09 +0200

    New design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,34 +16,34 @@
 	/>
 </svelte:head>
 
-<section class="hero relative w-full text-left flex flex-col items-center justify-center">
-	<div class="top w-[300px] relative w-full flex flex-row items-center justify-evenly flex-grow">
+<section class="hero relative w-full text-center sm:text-left flex flex-col items-center justify-center">
+	<div class="top w-[300px] px-5 relative w-full flex flex-col sm:flex-row items-center justify-evenly flex-grow">
 		<Stars />
+		<div class="relative flex items-center justify-center size-[200px] sm:size-[300px]">
+			<Moon class="size-[70%]" />
+		</div>
 		<div class="z-10">
 			<h1 class="text-4xl sm:text-6xl font-bold uppercase">Le cocon tech</h1>
-			<p class="sm:text-2xl p-5 max-w-2xl">
-				pour se motiver, s’entraider, progresser.<br> Ensemble.
+			<p class="sm:text-2xl py-5 max-w-2xl">
+				Pour se motiver, s’entraider, progresser.<br> Ensemble.
 			</p>
 			<Link label="Rejoins le Discord →" href="https://discord.gg/BAZdtgcuNX" target="_blank" />
-		</div>
-		<div class="relative flex items-center justify-center size-[200px] sm:size-[300px]">
-			<Moon class="size-[100%]" />
 		</div>
 	</div>
 	<div class="bottom bg-[var(--color-blue-night)] relative h-[23%] w-full flex flex-col items-center justify-center">
 		<div class="grass absolute top-0 start-0 size-full flex flex-row items-end justify-start">
-			<div class="element relative bg-[#1b2340] h-[130%] w-[25%]"></div>
-			<div class="element relative bg-[#1b2340] h-[130%] w-[25%]"></div>
-			<div class="element relative bg-[#1b2340] h-[130%] w-[55%]"></div>
+			<div class="element hidden sm:block relative bg-[#1b2340] h-[130%] w-[25%]"></div>
+			<div class="element hidden sm:block relative bg-[#1b2340] h-[130%] w-[45%]"></div>
+			<div class="element relative bg-[#1b2340] h-[130%] w-[100%] sm:w-[55%]"></div>
 		</div>
 		<div class="grass absolute top-0 start-0 size-full flex flex-row items-end justify-start">
-			<div class="element relative bg-[var(--background)] h-[100%] w-[40%]"></div>
-			<div class="element relative bg-[var(--background)] h-[100%] w-[60%]"></div>
+			<div class="element hidden sm:block relative bg-[var(--background)] h-[100%] w-[40%]"></div>
+			<div class="element relative bg-[var(--background)] h-[100%] w-[100%] sm:w-[60%]"></div>
 		</div>
 		<Tree class="hidden sm:block absolute bottom-[85%] -start-[5%] w-[400px] h-[520px] scale-[70%] sm:scale-100" />
-		<Plant class="hidden sm:block absolute bottom-[85%] end-[40%] w-[60px] scale-[70%] sm:scale-100 overflow-hidden" />
-		<Plant class="hidden sm:block absolute bottom-[85%] end-[2%] w-[100px] scale-[70%] sm:scale-100 overflow-hidden" />
-		<!-- <Raccoon class="absolute bottom-[60%] end-[0] sm:end-[15%] w-[170px] sm:w-[215px]" /> -->
+		<Plant class="absolute bottom-[85%] end-[40%] w-[60px] scale-[70%] sm:scale-100 overflow-hidden" />
+		<Plant class="absolute bottom-[55%] sm:bottom-[85%] end-[2%] w-[100px] scale-[70%] sm:scale-100 overflow-hidden" />
+		<!-- <Raccoon class="absolute bottom-[60%] start-[0] sm:end-[15%] w-[170px] sm:w-[215px]" /> -->
 	</div>
 </section>
 
