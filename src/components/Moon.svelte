48c1816ea907fc5c07e7148be3bfd58549f9b2aa
Angèle Henry <angele.henry@outlook.com>
2025-03-31 21:11:34 +0200

    Moon

--- src/components/Moon.svelte
+++ src/components/Moon.svelte
@@ -1,4 +1,63 @@
 <div aria-hidden="true" {...$$restProps}>
+  <div class="moon relative size-full"></div>
+</div>
+
+<style scoped>
+  :root {
+    --moon-color-dark: var(--color-white);
+    --shadow-color: 107, 121, 194;
+  }
+
+  .moon {
+    background-color: #ffffff;
+    border-radius: 50%;
+  }
+  .moon:before {
+    content: "";
+    position: absolute;
+    background-color: var(--moon-color-dark);
+    height: 30%;
+    width: 30%;
+    border-radius: 50%;
+    top: 20%;
+    left: 15%;
+  }
+  .moon:after {
+    content: "";
+    position: absolute;
+    background-color: var(--moon-color-dark);
+    height: 15%;
+    width: 15%;
+    border-radius: 50%;
+    top: 60%;
+    left: 40%;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .moon {
+      animation: pulseGlow 5s infinite ease-in-out;
+    }
+    @keyframes pulseGlow {
+      0% {
+          box-shadow: 0 0 80px rgba(var(--shadow-color), 0.6),
+                      0 0 150px rgba(var(--shadow-color), 0.4),
+                      0 0 250px rgba(var(--shadow-color), 0.2);
+      }
+      50% {
+          box-shadow: 0 0 100px rgba(var(--shadow-color), 0.8),
+                      0 0 180px rgba(var(--shadow-color), 0.5),
+                      0 0 300px rgba(var(--shadow-color), 0.3);
+      }
+      100% {
+          box-shadow: 0 0 80px rgba(var(--shadow-color), 0.6),
+                      0 0 150px rgba(var(--shadow-color), 0.4),
+                      0 0 250px rgba(var(--shadow-color), 0.2);
+      }
+    }
+  }
+</style>
+
+<!-- <div aria-hidden="true" {...$$restProps}>
   <div class="moon size-full"></div>
 </div>
 
@@ -33,4 +92,4 @@
       }
     }
   }
-</style>
+</style> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 	<div class="top w-[300px] px-5 relative w-full flex flex-col sm:flex-row items-center justify-evenly flex-grow">
 		<Stars />
 		<div class="relative flex items-center justify-center size-[200px] sm:size-[300px]">
-			<Moon class="size-[70%]" />
+			<Moon class="relative size-[200px]" />
 		</div>
 		<div class="z-10">
 			<h1 class="text-4xl sm:text-6xl font-bold uppercase">Le cocon tech</h1>
