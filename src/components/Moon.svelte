344d8f23b6fe7574deefccaebad68874e95a4fab
Angèle Henry <angele.henry@outlook.com>
2025-04-01 13:48:32 +0200

    Improve moon

--- src/components/Moon.svelte
+++ src/components/Moon.svelte
@@ -1,36 +1,16 @@
 <div aria-hidden="true" {...$$restProps}>
-  <div class="moon relative size-full"></div>
+  <div class="moon relative size-full bg-[var(--moon-color)] rounded-full">
+    <div class="absolute top-[20%] start-[15%] bg-[var(--moon-color-dark)] size-[30%] rounded-full"></div>
+    <div class="absolute top-[60%] start-[40%] bg-[var(--moon-color-dark)] size-[15%] rounded-full"></div>
+    <div class="absolute top-[40%] start-[50%] bg-[var(--moon-color-dark)] size-[10%] rounded-full"></div>
+  </div>
 </div>
 
 <style scoped>
   :root {
     --moon-color-dark: var(--color-white);
+    --moon-color: #ffffff;
     --shadow-color: 107, 121, 194;
-  }
-
-  .moon {
-    background-color: #ffffff;
-    border-radius: 50%;
-  }
-  .moon:before {
-    content: "";
-    position: absolute;
-    background-color: var(--moon-color-dark);
-    height: 30%;
-    width: 30%;
-    border-radius: 50%;
-    top: 20%;
-    left: 15%;
-  }
-  .moon:after {
-    content: "";
-    position: absolute;
-    background-color: var(--moon-color-dark);
-    height: 15%;
-    width: 15%;
-    border-radius: 50%;
-    top: 60%;
-    left: 40%;
   }
 
   @media (prefers-reduced-motion: no-preference) {
@@ -56,40 +36,3 @@
     }
   }
 </style>
-
-<!-- <div aria-hidden="true" {...$$restProps}>
-  <div class="moon size-full"></div>
-</div>
-
-<style scoped>
-  :root {
-    --moon-color: var(--color-white);
-    --shadow-color: 107, 121, 194;
-  }
-  .moon {
-    background-color: var(--moon-color);
-    border-radius: 50%;
-  }
-  @media (prefers-reduced-motion: no-preference) {
-    .moon {
-      animation: pulseGlow 5s infinite ease-in-out;
-    }
-    @keyframes pulseGlow {
-      0% {
-          box-shadow: 0 0 80px rgba(var(--shadow-color), 0.6),
-                      0 0 150px rgba(var(--shadow-color), 0.4),
-                      0 0 250px rgba(var(--shadow-color), 0.2);
-      }
-      50% {
-          box-shadow: 0 0 100px rgba(var(--shadow-color), 0.8),
-                      0 0 180px rgba(var(--shadow-color), 0.5),
-                      0 0 300px rgba(var(--shadow-color), 0.3);
-      }
-      100% {
-          box-shadow: 0 0 80px rgba(var(--shadow-color), 0.6),
-                      0 0 150px rgba(var(--shadow-color), 0.4),
-                      0 0 250px rgba(var(--shadow-color), 0.2);
-      }
-    }
-  }
-</style> -->
