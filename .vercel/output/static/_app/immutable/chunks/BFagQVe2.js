5c8e878a1e7569a8a50603aa90fd4be892481a80
Angèle Henry <angele.henry@outlook.com>
2025-04-05 00:20:43 +0200

    En français please

--- .vercel/output/static/_app/immutable/chunks/BFagQVe2.js
+++
@@ -1,6 +0,0 @@
-import{t as l,a as n}from"./CedpyJ0g.js";import{t as m,c as s,r as b}from"./Vs5WFrwu.js";import{s as c}from"./MX9IfiOI.js";import{s as r,a as f}from"./avF2M6tk.js";import{p as g}from"./hy54PhgO.js";var v=l("<a> </a>");function k(i,a){let e=g(a,"color",3,"primary");var t=v(),o=s(t,!0);b(t),m(()=>{r(t,"href",a.href),r(t,"title",a.target==="_blank"?"Opens in a new window":a.label),r(t,"aria-label",a.target==="_blank"?"Opens in a new window":a.label),r(t,"target",a.target),f(t,1,`inline-block mt-6 px-6 py-3 
-        rounded-full font-semibold 
-        text-[var(--background)] hover:text-[var(--background)] 
-        ${(e()==="primary"?"bg-[var(--primary)]":"bg-[var(--secondary-light)]")??""} 
-        ${(e()==="primary"?"hover:bg-[var(--secondary-light)]":"hover:bg-[var(--primary)]")??""} 
-        transition z-20`),c(o,a.label)}),n(i,t)}export{k as L};
