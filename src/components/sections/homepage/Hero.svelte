867878a2498112cdbf263e67d91e5cedf9d8be11
Angèle Henry <angele.henry@outlook.com>
2025-05-15 14:23:16 +0200

    Possible heading fixed

--- src/components/sections/homepage/Hero.svelte
+++ src/components/sections/homepage/Hero.svelte
@@ -6,10 +6,10 @@
 	<div></div>
 	<div>
 		<h1 class="mt-20 md:mt-0 text-2xl lg:text-4xl font-bold uppercase">forabetter.tech</h1>
-		<p class="lg:text-2xl pt-5 max-w-2xl">
+		<h2>
 			Bienvenue dans l’open space virtuel de la tech.
-		</p>
-		<p class="lg:text-xl pb-5 max-w-2xl mb-5">
+		</h2>
+		<p class="pb-5 max-w-2xl mb-5">
 			Coworking, entraide, formations et ressources pour progresser en toute bienveillance.
 		</p>
 		<Link 
